a2b1118d9b710f1ce082dd02297a03b8f645c9db
Celine86 <celine.obringer@live.fr>
2024-08-18 22:58:21 +0200

    Fetch Posts

---
+++ scripts/fetchPosts.js
@@ -0,0 +1,25 @@
+async function showPosts() {
+    try {
+        const response  = await fetch(`${urlApi}/posts/all`, {
+            method: 'GET',
+        })
+        if(response.status === 200) { 
+            const data = await response.json();
+            const allPosts = data.allPosts;
+            console.log(data)
+            for (let post in allPosts){
+                post = allPosts[post]
+                const templatePosts = document.getElementById('templatePosts')
+                const seePost = document.importNode(templatePosts.content, true)
+                seePost.getElementById('postTitle').textContent = post.title
+                seePost.getElementById('postContent').textContent = (post.content.length > 120 ? post.content.slice(0, 120) + '...' : post.content)
+                document.getElementById('allPosts').appendChild(seePost)  
+            }
+        } else {
+            console.log(response)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+showPosts();
