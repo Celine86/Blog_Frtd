e2601a6698ddbe456cb9c05a133e6a59afbbd21c
Celine86 <celine.obringer@live.fr>
2024-11-16 18:12:44 +0100

    Add logout

---
+++ scripts/logout.js
@@ -0,0 +1,10 @@
+document.getElementById('logout').addEventListener('click', async function logout(e) {
+    e.preventDefault();
+    sessionStorage.removeItem("email")
+    sessionStorage.removeItem("isAuthenticated")
+    sessionStorage.removeItem("loginmsg")
+    sessionStorage.removeItem("token")
+    sessionStorage.removeItem("useremail")
+    sessionStorage.removeItem("username")
+    window.location = 'index.html';
+}) 
